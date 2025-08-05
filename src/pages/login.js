7dbfa8a55a5fbb4c30aec3bccbab6d0f730a06cf
PAMELLA CHRISTINI PERETO E SILVA <pamella.cpesilva@senacsp.edu.br>
2025-08-05 17:42:24 -0300

    Manipulacao do Bootstrap na página de Login, criacao da div container para conter o formulario

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,10 +2,16 @@ import LoginForm from "../components/LoginForm.js";
 
 export default function renderLoginPage() { 
     const divRoot = document.getElementById('root');
-
     divRoot.innerHTML = '';
 
-    const formulario = LoginForm();
+    //Dentro de divRoot terá uma div chamada container e em container estará o formulario
+    const container = document.createElement('div');
+    container.className = 'card p-4 shadow-lg'; //Classe do Bootstrap para criar um card
+    container.style.width = '100%'; //Aplicada a largura de 100% na div container pra ocupar a tela
+    container.style.maxWidth = '400px'; //Até que atinja o máximo de 400px
+    divRoot.appendChild(container); //divRoot contém a nova div
 
-    divRoot.appendChild(formulario);
+    const formulario = LoginForm();
+    
+    container.appendChild(formulario); //Nova div container, já dentro de divRoot, contém o form
 }
