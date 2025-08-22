241fcb11f82f21d2915b331bcfe08058db74597b
PAMELLA CHRISTINI PERETO E SILVA <pamella.cpesilva@senacsp.edu.br>
2025-08-22 17:15:48 -0300

    implementado SPA hash routing

--- src/main.js
+++ src/main.js
@@ -4,17 +4,21 @@ import renderRegisterPage from "./pages/register.js";
 //Configuraçao de rotas mapeadas
 const routes = {
    "/login": renderLoginPage,
-   "/register": renderRegisterPage
+   "/cadastro": renderRegisterPage
    //Novas páginas aqui adicionadas conforme desenvolvidas
 };
+
 
 //Obtém o caminho atual a partir do hash da URL
 function getPath() {
    //obtém o hash (ex. "#/login"), remove o # e tira espaços
    const url = (location.hash || "").replace(/^#/, "").trim();
-   //retorna url se começar com "/", se não, retorna "/login" como padrão
+   console.log(url);
+
+   //retorna url se começar com "/", se não, retorna "/login" como padrão, posteriormente será /home
    return url && url.startsWith("/") ? url : "/login";         
 }
+
 
 //Decide o que renderizar com base na rota atual
 function renderRoutes() {
@@ -23,6 +27,9 @@ function renderRoutes() {
    render(); //Executa a função de render na página atual
 }
 
+
 window.addEventListener("hashchange", renderRoutes);
 //Renderizacao
 document.addEventListener('DOMContentLoaded', renderRoutes);
+
+console.log(routes);
